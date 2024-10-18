e7e209efb5fa09db532a8aea4b6eaf4de9e02c74
Shelby Falde <theseventhdegree7@gmail.com>
2024-10-18 12:45:30 -0700

    updated interpreter

--- interpreterv1.py
+++ interpreterv1.py
@@ -20,9 +20,9 @@ class Interpreter(InterpreterBase):
         func_list = ast.dict['functions'] 
 
         # checks for function whose name is 'main'
-        for i in range(0,len(func_list)):
-            if func_list[i].dict['name'] == "main":
-                return func_list[i]
+        for func in func_list:
+            if func.dict['name'] == "main":
+                return func
         
         # define error for 'main' not found.
         super().error(ErrorType.NAME_ERROR, "No main() function was found")
@@ -32,6 +32,7 @@ class Interpreter(InterpreterBase):
     def run_func(self, func_node):
         # statements key for sub-dict.
         for statement_node in func_node.dict['statements']:
+            # self.output(statement_node)
             self.run_statement(statement_node)
     
 
@@ -44,29 +45,52 @@ class Interpreter(InterpreterBase):
             self.do_func_call(statement_node)
     
     def is_definition(self, statement_node):
-        raise NotImplementedError
-
+        return (True if statement_node.elem_type == "vardef" else False)
     def is_assignment(self, statement_node):
-        raise NotImplementedError
-    
+        return (True if statement_node.elem_type == "=" else False)
     def is_func_call(self, statement_node):
-        raise NotImplementedError
+        return (True if statement_node.elem_type == "fcall" else False)
+
+
 
     def do_definition(self, statement_node):
-        raise NotImplementedError
+        # just add to var name
+
+        target_var_name = self.get_target_variable_name(statement_node)
+        self.variable_name_to_value[target_var_name] = None
+        
 
     def do_assignment(self, statement_node):
-        raise NotImplementedError
+        pass
+        target_var_name = self.get_target_variable_name(statement_node)
+        # self.output(target_var_name)
+        if not self.var_name_exists(target_var_name):
+            super().error(ErrorType.NAME_ERROR, "variable used and not declared", )
+        source_node = self.get_expression_node(statement_node)
+        resulting_value = self.evaluate_expression(source_node)
+        self.variable_name_to_value[target_var_name] = resulting_value
+        
+        self.output(source_node.elem_type)
+    
+    def get_target_variable_name(self, statement_node):
+        return statement_node.dict['name']
+    def var_name_exists(self, varname):
+        return True if varname in self.variable_name_to_value.keys() else False
+    
+    def get_expression_node(self, statement_node):
+        return statement_node.dict['expression']
     
     def do_func_call(self, statement_node):
-        raise NotImplementedError
+        pass
     
 
-    def evaluate_expression(expression_node):
-        raise NotImplementedError
+    def evaluate_expression(self, expression_node):
+
+        pass
+        # raise NotImplementedError
     
     # + or -
-    def evaluate_binary_operator(expression_node):
+    def evaluate_binary_operator(self, expression_node):
         raise NotImplementedError
 
     # Several more functions remain
@@ -75,7 +99,7 @@ class Interpreter(InterpreterBase):
 program = """
             func main() {
              var x;
-             x = 5 + 6;
+             x = 5 + 6 - 7;
              print("The sum is: ", x);
             }"""
 
